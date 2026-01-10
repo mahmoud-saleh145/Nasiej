bc868161b0d5352d0af43876e8cd87b95f8b6269
mahmoud-saleh145 <salehmahmoud327@gmail.com>
2026-01-11 00:12:34 +0200

    fix meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '25531279009897019');
+              fbq('init', '1212792433550225');
               fbq('track', 'PageView');
             `,
           }}
